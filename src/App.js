6c271447dc322a7c1a99954d290f8e94cdc59513
Suman Maji <sumanmaji349@gmail.com>
2023-03-15 22:11:54 +0530

    Corrections in index.html and App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 import React from 'react';
 import './App.css';
 import Main from './components/Main';
@@ -31,13 +9,8 @@ function App() {
        <Head/>
       <Navbar/>
       <Main />
->>>>>>> 688a34a12af1bab5b578ec9190ea2ed089101206
     </div>
   );
 }
 
-<<<<<<< HEAD
 export default App;
-=======
-export default App;
->>>>>>> 688a34a12af1bab5b578ec9190ea2ed089101206
