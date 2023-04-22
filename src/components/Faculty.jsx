a0400c758f9d0cb7558154b4e9a333ebd3542cb8
NilayPal <nilaypal96@gmail.com>
2023-04-22 20:51:29 +0530

    SUMAN AHHHHHH

--- src/components/Faculty.jsx
+++ src/components/Faculty.jsx
@@ -16,44 +16,44 @@ function Faculty() {
 
  <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/atanu_banerjee.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
+        <span className='cardname'>Dr. Atanu Banerjee<br></br>Assistant Professor of</span>
           <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+          <p>B.Sc (Phy. Hons.), B.E., M.E., Ph.D.(Engg.)(20 years experience) <br></br>DOJ:15-03-2007</p>
       </div>
    </div>
  </div>   
 {/* For teacher 2 */}
  <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/joydeep_banerjee.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
+        <span className='cardname'>Dr. Joydeep Banerjee<br></br>Assistant Professor of</span>
           <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+          <p>B.Tech, M.Tech, Ph.D.(Engg.)(13 years experience) <br></br>DOJ: 10-02-2009</p>
       </div>
    </div>
  </div>   
 {/* For teacher 3 */}
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/subhasish_banerjee.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
+        <span className='cardname'>Mr. Subhasish Banerjee<br></br>Assistant Professor of</span>
           <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+          <p>B.Tech, M.Tech, Pursuing Ph.D. (Tech.)(17.5 years experience) <br></br>DOJ: 20-02-2007</p>
       </div>
    </div>
  </div>   
 {/* For teacher 4 */}
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/kalyan_biswas.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
+        <span className='cardname'>Dr. Kalyan Biswas<br></br>Assistant Professor of</span>
           <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+          <p>B.Tech,M.Tech,Ph.D.(Engg.)(14 years experience(teaching),6 years(industry),2.5years(research)) <br></br>DOJ: 17-03-2009</p>
       </div>
    </div>
  </div>   
@@ -64,44 +64,44 @@ function Faculty() {
 {/* For teacher 1 of row 2 */}  
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/krishendu_chattopadhyay.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
+        <span className='cardname'>Dr. Krishnendu Chattopadhyay<br></br>Assistant Professor of</span>
           <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+          <p>AMIETE, M.E., Ph.D.(Engg.)(22 years experience) <br></br>DOJ: 01-03-2001</p>
       </div>
    </div>
  </div>   
 {/* For teacher 2 of row 2 */}
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="https://lsplwebsitedata.s3.ap-south-1.amazonaws.com/mckvi-wp-data/wp-content/uploads/2022/04/08135251/ECE_SDey_Min-1.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
+        <span className='cardname'>Ms. Sushmita Dey<br></br>Assistant Professor of</span>
           <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+          <p>AMIE, M.E.(3 years experience) <br></br>DOJ: 02-01-2020</p>
       </div>
    </div>
  </div>   
 {/* For teacher 3 of row 2 */}
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/swarup_kr_mitra.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
+        <span className='cardname'>Dr. Swarup Kr. Mitra<br></br>Professor and HOD of</span>
           <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+          <p>B.Tech, M.Tech, Ph.D.(Engg.)(20 years experience) <br></br>DOJ: 01-03-2008</p>
       </div>
    </div>
  </div>   
 {/* For teacher 4 of row 2 */}
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/soham_lodh.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
+        <span className='cardname'>Mr.Soham Lodh<br></br>Assistant Professor of</span>
           <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+          <p>B.Tech, M.Tech, Pursuing Ph.D. (Tech.)(11.5 years experience) <br></br>DOJ: 19-02-2011</p>
       </div>
    </div>
  </div>   
@@ -116,44 +116,44 @@ function Faculty() {
 
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="https://lsplwebsitedata.s3.ap-south-1.amazonaws.com/mckvi-wp-data/wp-content/uploads/2021/06/03150127/Soma-Bandyopadhyay-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
-          <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+        <span className='cardname'>Mrs. Soma Bandyopadhyay<br></br>Assistant Professor of</span>
+          <p className='cardtitle'>Computer Science and Engineering</p>
+          <p>M.E., Ph.D.(Pursuing)13 years 8 months experience in teaching(10 years 4 months experience) <br></br>DOJ: 25-07-2008</p>
       </div>
    </div>
  </div>   
 {/* For teacher 2  of row 1 of cse*/}
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/subir_bhadra.150x0-is-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
-          <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+        <span className='cardname'>Mr. Subir Bhadra<br></br>Assistant Professor of</span>
+          <p className='cardtitle'>Computer Science and Engineering</p>
+          <p>M.Sc. M.Tech(10.8 years experience) <br></br>DOJ: 01-07-2021</p>
       </div>
    </div>
  </div>   
 {/* For teacher 3  of row 1 of cse*/}
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/surojit_bhattacharya.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
-          <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+        <span className='cardname'>Mr. Surojit Bhattacharyya<br></br>Assistant Professor of</span>
+          <p className='cardtitle'>Computer Science and Engineering</p>
+          <p>B.Tech,M.E.,Ph.D(pursuing)(14.3 years experience) <br></br>DOJ: 03-08-2010</p>
       </div>
    </div>
  </div>   
 {/* For teacher 4  of row 1 of cse*/}
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/avijit_bose.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
-          <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+        <span className='cardname'>Mr. Avijit Bose<br></br>Assistant Professor of</span>
+          <p className='cardtitle'>Computer Science and Engineering</p>
+          <p>B.E.,M.Tech,Ph.D(pursuing)(21 years experience) <br></br>DOJ: 13-05-2003</p>
       </div>
    </div>
  </div>   
@@ -165,44 +165,44 @@ function Faculty() {
 {/* For teacher 1 of row 2 of cse*/}  
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/prasenjit_das.200x0-1-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
-          <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+        <span className='cardname'>Mr. Prasenjit Das<br></br>Assistant Professor of</span>
+          <p className='cardtitle'>Computer Science and Engineering</p>
+          <p>B.Sc., MCA, M.Tech(12.4 years experience) <br></br>DOJ: 01-08-2008</p>
       </div>
    </div>
  </div>   
 {/* For teacher 2 of row 2 of cse*/}  
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="http://www.mckvie.edu.in/wp-content/uploads/2021/06/rachita_ghoshhazra.200x0-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
-          <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+        <span className='cardname'>Ms. Rachita Ghoshhajra<br></br>Assistant Professor of</span>
+          <p className='cardtitle'>Computer Science and Engineering</p>
+          <p>B.Tech, M.Tech(17 years experience) <br></br>DOJ: 17-03-2009</p>
       </div>
    </div>
  </div>   
 {/* For teacher 3 of row 2 of cse*/}  
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="https://lsplwebsitedata.s3.ap-south-1.amazonaws.com/mckvi-wp-data/wp-content/uploads/2022/08/03145908/MGoswami-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
-          <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+        <span className='cardname'>Ms. Moumita Goswami<br></br>Assistant Professor of</span>
+          <p className='cardtitle'>Computer Science and Engineering</p>
+          <p>B.Tech, M.Tech, Ph.D (pursuing)(11.5 years experience) <br></br>DOJ: 27-07-2012</p>
       </div>
    </div>
  </div>   
 {/* For teacher 4 of row 2 of cse*/}  
 <div className='cards'>
    <div className='card'>
-    <img src="https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" alt="mypic"></img>
+    <img src="https://lsplwebsitedata.s3.ap-south-1.amazonaws.com/mckvi-wp-data/wp-content/uploads/2022/08/03152625/PuspenLahiri2-1-150x150.jpg" alt="mypic"></img>
       <div className='cardinfo'>
-        <span className='cardname'>Mr.Shom Lodh<br></br>Assistant Professor of</span>
-          <p className='cardtitle'>Electronics And Communication Engineering</p>
-          <p>B.Tech, M.E.(8 years experience) <br></br>D.O.J.: 15.07.11</p>
+        <span className='cardname'>Mr. Puspen Lahiri<br></br>Assistant Professor of</span>
+          <p className='cardtitle'>Computer Science and Engineering</p>
+          <p>B.Tech, M.Tech(16.7 years experience) <br></br>DOJ: 14-08-2012</p>
       </div>
    </div>
  </div>   
