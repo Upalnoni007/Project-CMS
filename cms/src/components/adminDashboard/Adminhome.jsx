f6c8019e27c2542fa5e01d6d6d061c16ff5e7aa2
Upal Mitra <upalmitra007@gmail.com>
2024-05-26 12:20:37 +0530

    Nilay

--- cms/src/components/adminDashboard/Adminhome.jsx
+++ cms/src/components/adminDashboard/Adminhome.jsx
@@ -24,7 +24,7 @@ const Adminhome = () => {
                 <Card className='cards px-1'>
                     <Card.Img className='facultyImg mx-auto' variant="top" src="https://img.freepik.com/free-vector/study-abroad-concept-illustration_114360-7493.jpg?w=740&t=st=1681049478~exp=1681050078~hmac=59d0ffa5620b2a7bf0d76f17c77cd1afd724bd9216cf506c1b4b53531a163851" alt="2" />
                     <Card.Body>
-                        <NavLink className={'links'} to='/*'> <Card.Title>Students</Card.Title></NavLink>
+                        <NavLink className={'links'} to='/student'> <Card.Title>Students</Card.Title></NavLink>
                     </Card.Body>
                 </Card>
 
@@ -40,6 +40,13 @@ const Adminhome = () => {
                     <Card.Img className='facultyImg mx-auto' variant="top" src="https://img.freepik.com/premium-vector/exam-results-with-excellent-unsatisfactory-grades-paper-with-knowledge-assessment-best-worst-estimation-isolated-cartoon-set_91248-744.jpg?w=740" alt="4" />
                     <Card.Body>
                         <NavLink className={'links'} to='/result'> <Card.Title>Result</Card.Title></NavLink>
+
+                    </Card.Body>
+                </Card>
+                <Card className='cards px-1'>
+                    <Card.Img className='facultyImg mx-auto' variant="top" src="https://img.freepik.com/premium-vector/exam-results-with-excellent-unsatisfactory-grades-paper-with-knowledge-assessment-best-worst-estimation-isolated-cartoon-set_91248-744.jpg?w=740" alt="4" />
+                    <Card.Body>
+                        <NavLink className={'links'} to='/result'> <Card.Title>Payment</Card.Title></NavLink>
 
                     </Card.Body>
                 </Card>
